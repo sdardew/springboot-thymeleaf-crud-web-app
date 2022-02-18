670aa41c99fb789c6b122d36a23ef46edd95a446
sdardew <sdardew@gmail.com>
2022-02-18 15:40:27 +0900

    feat: update the employee information

--- src/main/java/com/example/springbootthymeleafcrudwebapp/service/EmployeeServiceImpl.java
+++ src/main/java/com/example/springbootthymeleafcrudwebapp/service/EmployeeServiceImpl.java
@@ -6,9 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
-public class EmployeeServiceImpl implements EmployeeService{
+public class EmployeeServiceImpl implements EmployeeService {
 
     @Autowired
     private EmployeeRepository employeeRepository;
@@ -21,5 +22,16 @@ public class EmployeeServiceImpl implements EmployeeService{
     @Override
     public void saveEmployee(Employee employee) {
         this.employeeRepository.save(employee);
+    }
+
+    @Override
+    public Employee getEmployeeById(long id) {
+        Optional<Employee> optional = employeeRepository.findById(id);
+        if(optional.isPresent()) {
+           return optional.get();
+        }
+        else {
+            throw new RuntimeException("Employee not found for id :: " + id);
+        }
     }
 }
